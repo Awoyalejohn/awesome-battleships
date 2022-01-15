960caaf05fc81185078b5a9c7622c47a18335bbd
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2022-01-15 20:13:28 +0000

    Added a welcome message

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 import random
 
+
+
+print("Welcome to Awesome Battleships!")
+print("Player and Computer Board size: 5")
+print("Number of ships: 4")
+print("from the top left corner")
+print("Row: 0")
+print("Column: 0")
+
+
