f19ee578cb3d90a58ec3a8fa7140cdf37f2f0ec6
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2022-01-16 14:28:53 +0000

    Add welcome function

--- run.py
+++ run.py
@@ -11,22 +11,25 @@ class PlayerGrid:
         self.player_side = player_side
         self.player_ships = player_ships
 
+
 class ComputerGrid:
     """
     Computer Grid class that holds information on the player,
     their side and ships
     """
-    def __init__(self, computer, computer_side, computer_ships ):
+    def __init__(self, computer, computer_side, computer_ships):
         self.computer = computer
         self.computer_side = computer_side
         self.computer_ships = computer_ships
 
 
+def welcome():
+    print("Welcome to Awesome Battleships!")
+    print("Player and Computer Board size: 5")
+    print("Number of ships: 4")
+    print("from the top left corner")
+    print("Row: 0")
+    print("Column: 0")
 
 
-print("Welcome to Awesome Battleships!")
-print("Player and Computer Board size: 5")
-print("Number of ships: 4")
-print("from the top left corner")
-print("Row: 0")
-print("Column: 0")
+welcome()
