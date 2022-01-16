42f7db5b33a44920dbf5034c272218407e7ab747
Awoyalejohn <88907733+Awoyalejohn@users.noreply.github.com>
2022-01-16 14:17:59 +0000

    Add a class for computer and player grids

--- run.py
+++ run.py
@@ -1,6 +1,28 @@
 import random
 
 
+class PlayerGrid:
+    """
+    Player Grid class that holds information on the player,
+    their side and ships
+    """
+    def __init__(self, player, player_side, player_ships):
+        self.player = player
+        self.player_side = player_side
+        self.player_ships = player_ships
+
+class ComputerGrid:
+    """
+    Computer Grid class that holds information on the player,
+    their side and ships
+    """
+    def __init__(self, computer, computer_side, computer_ships ):
+        self.computer = computer
+        self.computer_side = computer_side
+        self.computer_ships = computer_ships
+
+
+
 
 print("Welcome to Awesome Battleships!")
 print("Player and Computer Board size: 5")
@@ -8,5 +30,3 @@ print("Number of ships: 4")
 print("from the top left corner")
 print("Row: 0")
 print("Column: 0")
-
-
