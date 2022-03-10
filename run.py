c431fbde72be1d94d1347f999ef42a62b7948bb5
John Awoyale <awoyalejohn@gmail.com>
2022-03-10 19:48:40 +0000

    add ship row index for player and import random randint and random sample functions

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import random
+from random import randint, sample
+score = {"Player": 0, "Computer": 0}
 class Game:
     """ The game class that is reponsible for the objects that will be in the game"""
-    score = {"Player": 0, "Computer": 0}
     info = """Welcome to Awesome Battleships! 
     Player and Computer Board size: 
     5 Number of ships: 4
@@ -19,13 +19,6 @@ class Game:
 
 
 #code from code instute battleship demo
-class Player(Game):
-    """
-    Player Grid class that holds information on the player,
-    their side and ships
-    """
-    pass
-#code from code instute battleship demo
 class Computer(Game):
     """
     Computer Grid class that holds information on the player,
@@ -33,17 +26,36 @@ class Computer(Game):
     """
     pass
 
+#code from code instute battleship demo
+class Player(Game):
+    """
+    Player Grid class that holds information on the player,
+    their side and ships
+    """
+    pass
+
+player_ship_row = sample(range(5), 4)
+player_ship_col = sample(range(5), 4)
+print(player_ship_row)
+print(player_ship_col)
+
+
 def display_grid(grid):
     for i in grid:
         print(" ".join(i))
 
 print(Game.info)
+print(score)
 hidden_grid = Game([])
 player_grid = Player([])
 computer_grid = Computer([])
 hidden_grid = hidden_grid.create_grid()
 player_grid = player_grid.create_grid()
 computer_grid = computer_grid.create_grid()
+print("Hidden Grid")
 display_grid(hidden_grid)
-display_grid(player_grid)
+print("Computer Grid")
 display_grid(computer_grid)
+print("Player Grid")
+display_grid(player_grid)
+
